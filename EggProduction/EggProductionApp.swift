9fce597b5950d340df1967251ee6ecdf1cebf923
keshew <artyom.korotckov2017@yandex.ru>
2025-04-09 18:28:09 +0500

    Initial Commit

---
+++ EggProduction/EggProductionApp.swift
@@ -0,0 +1,17 @@
+//
+//  EggProductionApp.swift
+//  EggProduction
+//
+//  Created by Артём Коротков on 09.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct EggProductionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
